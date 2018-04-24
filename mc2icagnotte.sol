01fb48fc49a6a4b7a6bfb0d76a7443cded33ab73
Rev909 <Rev909@users.noreply.github.com>
2018-04-24 17:53:59 +0200

    beta
     - Optimisation des fonctions de création et contribution
     - Suppression de la fonction de getCagnotte -> passage par le getter du mapping
     - Get des contributions en beta : uniquement la liste des adresses et des contributions sont retournées
     - Ajout des ENSF + commentaires

--- mc2icagnotte.sol
+++ mc2icagnotte.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.23;
-pragma experimental ABIEncoderV2;
 
+
+/// @title Smart-Contract de gestion des cagnottes des consultants mc²i
+/// @author Mathias EDOUIN
 contract mc2iCagnotte {
 
     struct Cagnotte {
@@ -8,6 +10,7 @@ contract mc2iCagnotte {
         address owner;
         string nom;
         uint montant;
+        uint nbreContributions;
         uint date;
         bool statut;
     }
@@ -20,74 +23,82 @@ contract mc2iCagnotte {
         string mot;
     }
     
-    mapping(uint => Cagnotte) IdToCagnotte;
+    event CreationCagnotte(uint ID, string nom, uint montant, uint nbreContrib, bool statut);
+    event ContributionCagnotte(uint IDCagnotte, uint montant, string nom, string mot);
+    
+    //On met le mapping en public les fonctions set/get
+    mapping(uint => Cagnotte) public getCagnotteByID;
+    
     
     Cagnotte[] cagnottes;
     Contribution[] contributions;
     
+    //Variable globale d'ID de cagnottes
     uint idCagnotte = 1;
 
-    /// Créer une nouvelle cagnotte initialisée à 0
+    /// @notice Crée la cagnotte avec seulement un nom
+    /// @param _nom Le nom de la cagnotte
     function CreerCagnotte(string _nom) public {
-        Cagnotte memory _cagnotte = Cagnotte(idCagnotte, msg.sender, _nom, 0, now, true);
+        Cagnotte memory _cagnotte = Cagnotte(idCagnotte, msg.sender, _nom, 0, 0, now, true);
+        //On met la cagnotte dans le tableau global
         cagnottes.push(_cagnotte);
-        IdToCagnotte[idCagnotte] = _cagnotte;
+        //On pousse la cagnotte dans le mapping
+        getCagnotteByID[idCagnotte] = _cagnotte;
+        //On envoie l'event pour le front
+        emit CreationCagnotte(idCagnotte, _nom, 0, 0, true);
+        //On incrémente la variable pour la cagnotte suivante
         idCagnotte++;
     }
 
-    /// Retirer l'argent d'une cagnotte
+    /// @notice Retire le montant d'une cagnotte dont on est le propriétaire, entraînant sa fermeture
+    /// @param _id L'ID de la cagnotte
     function RetirerCagnotte(uint _id) public {
-        require (IdToCagnotte[_id].owner == msg.sender, "Vous n'êtes pas le propriétaire de la cagnotte");
-        for (uint i = 0 ; i <= cagnottes.length ; i++) {
-            if (cagnottes[i].id == _id) {
-                msg.sender.transfer(cagnottes[i].montant);
-                cagnottes[i].statut = false;
-            }
-        }
+        //La personne qui doit retirer la cagnotte doit être son propriétaire
+        require (getCagnotteByID[_id].owner == msg.sender, "Vous n'êtes pas le propriétaire de la cagnotte");
+        //On transfert le montant de la cagnotte à son propriétaire
+		msg.sender.transfer(getCagnotteByID[_id].montant);
+		//On ferme la cagnotte
+        getCagnotteByID[_id].statut = false;
         
     }
 
-    /// Contribuer à une cagnotte
-    function ContribuerCagnotte(uint _id, string _nom, string _mot) payable public returns (string) {
-        uint valeurContrib = msg.value * 1 ether;
-        require (IdToCagnotte[_id].statut == true, "La cagnotte est fermée, vous ne pouvez plus y contribuer");
-        require(msg.sender.balance > msg.value, "Vous n'avez pas assez d'ethers pour contribuer à la cagnotte");
-        Contribution memory _contribution = Contribution(_id, msg.sender, valeurContrib, _nom, _mot);
+    /// @notice Contribue à une cagnotte, en laissant un nom et un mot avec le montant
+    /// @param _id L'ID de la cagnotte
+    /// @param _nom Le nom du contributeur
+    /// @param _mot Le mot laissé par le contributeur
+    /// @return bool Succès de la contribution
+    function ContribuerCagnotte(uint _id, string _nom, string _mot) payable public returns (bool success) {
+        require (getCagnotteByID[_id].statut == true, "La cagnotte est fermée, vous ne pouvez plus y contribuer");
+        require (msg.value > 0, "Vous ne pouvez pas contribuer à hauteur de 0 mc2icoins");
+        Contribution memory _contribution = Contribution(_id, msg.sender, msg.value, _nom, _mot);
         contributions.push(_contribution);
-        for (uint j = 0 ; j <= cagnottes.length ; j++) {
-            if (cagnottes[j].id == _id) {
-                cagnottes[j].montant += msg.value;
-                return ("Merci de votre contribution !");
-            }
-        }
+        getCagnotteByID[_id].montant += msg.value;
+        getCagnotteByID[_id].nbreContributions += 1;
+        emit ContributionCagnotte(_id, msg.value, _nom, _mot);
+        return (true);
     }
 
-    /// Afficher une cagnotte
-    function getCagnotte(uint _id) public view returns (bool found, string error, address owner, string nom, uint montant, bool statut) {
-        for (uint i = 0 ; i <= cagnottes.length ; i++) {
-            if (cagnottes[i].id == _id) {
-                return (true,"", cagnottes[i].owner, cagnottes[i].nom, cagnottes[i].montant, cagnottes[i].statut);
-            }
-        }
-        return (false, "Cagnotte non trouvée",0x0, "", 0,false);
-    }
-
-    /// Afficher les contributions d'une cagnotte
-    function getContributionsCagnotte(uint _id) public view returns (address[10], uint[10], string[10], string[10]) {
-        address[10] memory _contributeurs;
-        uint[10] memory _montants;
-        string[10] memory _noms;
-        string[10] memory _mots;
+    /// @notice Permet d'obtenir chaque contribution relatives à une cagnotte
+    /// @dev Ne retourne pour l'instant que des tableaux d'adresses et d'int, une solution pour les noms et mots est en cours d'investigation
+    /// @param _id L'ID de la cagnotte
+    /// @return address[] Tableau des adresses des contributeurs
+    /// @return uint[] Tableau des montants des contributions
+    function getContributionsByCagnotte(uint _id) public view returns (address[], uint[]) {
+        //On réserve deux tableaux et on fixe leur taille avec le nombre de contributions de la cagnotte
+		address[] memory _contribs = new address[](getCagnotteByID[_id].nbreContributions);
+        uint[] memory _montants = new uint[](getCagnotteByID[_id].nbreContributions);
         uint counter = 0;
-        for (uint i = 0 ; i <= contributions.length ; i++) {
-            if (_id == contributions[i].CagnotteID) {
-                _contributeurs[counter] = contributions[i].sender;
-                _montants[counter] = contributions[i].montant;
-                _noms[counter] = contributions[i].nom;
-                _mots[counter] = contributions[i].mot;
+		//On itère dans le tableau des contributions, à la recherhe des contributions liées à la cagnotte
+        for (uint i = 0 ; i < contributions.length ; i++) {
+			//Dès qu'on en trouve une
+            if (contributions[i].CagnotteID == _id) {
+				//On charge ses paramètres dans les deux tableaux temporaires
+                Contribution storage contrib = contributions[i];
+                _contribs[counter] = contrib.sender;
+                _montants[counter] = contrib.montant;
                 counter++;
             }
         }
-        return(_contributeurs, _montants, _noms, _mots);
+        return(_contribs, _montants);
     }
 }
